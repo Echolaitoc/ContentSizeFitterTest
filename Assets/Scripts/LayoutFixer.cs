4da1cdb95e2d6dc9e84cf9e80a3bf86f62d68d8a
Tristan Weber <echo1990@gmx.de>
2018-06-22 10:48:48 +0200

    Initial commit

---
+++ Assets/Scripts/LayoutFixer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LayoutFixer : MonoBehaviour
+{
+    public void FixLayout()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FixLayoutCoroutine());
+    }
+
+    private IEnumerator FixLayoutCoroutine()
+    {
+        List<VerticalLayoutGroup> verticalLayoutGroups = new List<VerticalLayoutGroup>();
+        GetComponentsInChildren<VerticalLayoutGroup>(true, verticalLayoutGroups);
+
+        List<ContentSizeFitter> contentSizeFitters = new List<ContentSizeFitter>();
+        GetComponentsInChildren<ContentSizeFitter>(true, contentSizeFitters);
+
+        // --- Enable Layout Components ---
+
+        foreach (var vlg in verticalLayoutGroups)
+        {
+            vlg.enabled = true;
+        }
+
+        contentSizeFitters.Reverse();
+        foreach (var csf in contentSizeFitters)
+        {
+            if (csf.GetComponentsInChildren<ContentSizeFitter>().Length > 1)
+            {
+                yield return null; // if all content size fitters are updated in the same frame, they do not take into account changes in their children
+            }
+            csf.enabled = true;
+        }
+
+        yield return null;
+
+        // --- Disable Layout Components ---
+
+        foreach (var csf in contentSizeFitters)
+        {
+            csf.enabled = false;
+        }
+        foreach (var vlg in verticalLayoutGroups)
+        {
+            vlg.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TextToggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextToggle : MonoBehaviour
+{
+    [SerializeField, TextArea(3, 10)] private string textOne;
+    [SerializeField, TextArea(3, 10)] private string textTwo;
+
+    private TextMeshProUGUI textMesh;
+    private bool isTextOne = true;
+
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void ToggleText()
+    {
+        if (textMesh == null)
+        {
+            return;
+        }
+        isTextOne = !isTextOne;
+        textMesh.text = (isTextOne ? textOne : textTwo);
+    }
+}
